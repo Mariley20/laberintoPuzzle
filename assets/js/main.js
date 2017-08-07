5cf4fde8d1b39e2da35bb97af04722759966d83e
Mariley20 <mariley2018@gmail.com>
2017-08-06 21:24:26 -0500

    rotar imagen

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,21 +10,33 @@ var mapa=[
 "*_**_*__*****_**_*",
 "*o*__*________**W*",
 "******************"];
+var canvas = [];
 
 for (var i = 0; i < mapa.length; i++) {
 	var pared = mapa[i].split("");
+	canvas.push(pared);
+	//console.log(pared)
 	var tag_TR = document.createElement('tr');
 	for (var j = 0; j < pared.length; j++) {
 		var tag_TD = document.createElement('td');
 		var texto = document.createTextNode(pared[j])
+		tag_TD.setAttribute('id', i+""+j);
 		tag_TD.appendChild(texto);
 		var clase = '';
 		if(pared[j] == "*"){
 			clase = 'pared';
 		}else if(pared[j] == "_"){
 			clase = 'camino    ';
+		}else if (pared[j] == "o"){
+			clase = 'inicioFin';
+			var img = document.createElement('img');
+			img.setAttribute('src', 'assets/icn/flecha_arriba.png');
+			img.setAttribute('id', 'img');
+			//var value_TD = document.createTextNode(img);
+			tag_TD.appendChild(img);
+
 		}else{
-			clase = 'inicioFin'
+			clase = 'inicioFin';
 		}
 		tag_TD.setAttribute('class', clase);
 		tag_TR.appendChild(tag_TD);
@@ -32,18 +44,55 @@ for (var i = 0; i < mapa.length; i++) {
 	laberinto.appendChild(tag_TR);
 }
 
-var lastDownTarget, canvas;
-window.onload = function() {
-    canvas = document.getElementById('laberinto');
+var contadorRotando = 0;
+var btnRotarDerecha = document.getElementById('rotarDerecha')
+btnRotarDerecha.onclick = function(){
+	if(p == 1){contadorRotando++;
+		p = 0;
+	}
+	if(contadorRotando == 4){
+		contadorRotando = 0;
+	}
+	console.log(contadorRotando);
+	rotar(contadorRotando);
 
-    document.addEventListener('mousedown', function(event) {
-        lastDownTarget = event.target;
-        alert('mousedown');
-    }, false);
+	contadorRotando++;
 
-    canvas.addEventListener('keydown', function(event) {
-        //if(lastDownTarget == canvas) {
-            alert('keydown');
-        
-    }, false);
+}
+var p = 0;
+var btnRotarIzquierda = document.getElementById('rotarIzquierda');
+btnRotarIzquierda.onclick = function(){
+	var x=0;
+	if(p == 0){
+		(contadorRotando == 0)?p = 1:contadorRotando--; 
+		p = 1;
+	}
+
+	if(contadorRotando == 0){
+		contadorRotando = 4;
+	}
+	contadorRotando--;
+	console.log(contadorRotando)
+	rotar(contadorRotando);
+	
+	//
+}
+
+function rotar(c){
+	switch(c){
+		case 0:
+			var grados = "rotate(90deg)"; 
+			//c++;
+			break;
+		case 1: 
+			var grados = "rotate(180deg)";
+			break;
+		case 2: 
+			var grados = "rotate(270deg)";
+			break;
+		case 3: 
+			var grados = "rotate(360deg)";
+			break;
+	}
+	document.getElementById('img').style.transform = grados;
 }
