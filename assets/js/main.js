ad45f707d2df133bc541ede30511511117963559
Mariley20 <mariley2018@gmail.com>
2017-08-07 00:46:22 -0500

    jugamos tetriz

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,6 +32,7 @@ for (var i = 0; i < mapa.length; i++) {
 			var img = document.createElement('img');
 			img.setAttribute('src', 'assets/icn/flecha_arriba.png');
 			img.setAttribute('id', 'img');
+			img.setAttribute('style', 'transform:rotate(360deg)')
 			//var value_TD = document.createTextNode(img);
 			tag_TD.appendChild(img);
 
@@ -53,7 +54,7 @@ btnRotarDerecha.onclick = function(){
 	if(contadorRotando == 4){
 		contadorRotando = 0;
 	}
-	console.log(contadorRotando);
+	//console.log(contadorRotando);
 	rotar(contadorRotando);
 
 	contadorRotando++;
@@ -72,7 +73,7 @@ btnRotarIzquierda.onclick = function(){
 		contadorRotando = 4;
 	}
 	contadorRotando--;
-	console.log(contadorRotando)
+	//console.log(contadorRotando)
 	rotar(contadorRotando);
 	
 	//
@@ -95,4 +96,66 @@ function rotar(c){
 			break;
 	}
 	document.getElementById('img').style.transform = grados;
+}
+var btnAvanzar = document.getElementById('avanzar');
+btnAvanzar.onclick = function(){
+	var idTagPadre = document.getElementById('img').parentNode.id;
+	idTag = idTagPadre.split('');
+	if(idTag.length == 3){
+		idTag[1] = idTag[1]+""+idTag[2];
+	}
+	(idTagPadre == '916')?alert('Ganaste'):console.log(idTagPadre)
+	
+	var derecha   = idTag[0] +""+ (parseInt(idTag[1])+1);
+	var izquierda = idTag[0] +""+ (parseInt(idTag[1]) - 1);
+	var arriba    = (parseInt(idTag[0]) - 1) +""+ idTag[1];
+	var abajo     = (parseInt(idTag[0]) + 1) +""+ idTag[1];
+	var estilo    = document.getElementById('img').style.transform;
+	var derechaValor   = document.getElementById(derecha).textContent;
+	var izquierdaValor = document.getElementById(izquierda).textContent;
+	var arribaValor    = document.getElementById(arriba).textContent;
+	var abajoValor     = document.getElementById(abajo).textContent;
+	var direccion;
+	
+	switch(estilo){
+		case 'rotate(360deg)':
+
+			if(arribaValor != '*'){
+				direccion = arriba;
+				removeImg(direccion, idTagPadre, estilo);
+			}
+			break;
+		case 'rotate(180deg)':
+			if(abajoValor != '*'){
+				direccion = abajo;
+				removeImg(direccion, idTagPadre, estilo);
+			}
+			
+			break;
+		case 'rotate(270deg)':
+			if(izquierdaValor != '*'){
+				direccion = izquierda;
+				removeImg(direccion, idTagPadre, estilo);
+			}
+			break;
+		case 'rotate(90deg)' :
+			if(derechaValor != '*'){
+				
+				direccion = derecha;
+				removeImg(direccion, idTagPadre, estilo);
+			}
+			break;
+	}
+		
+}
+
+function removeImg(direccion, idTagPadre,estilo){
+	var contenedorImg = document.getElementById(idTagPadre);
+				contenedorImg.removeChild(contenedorImg.childNodes[1]);
+	var img = document.createElement('img');
+				img.setAttribute('src', 'assets/icn/flecha_arriba.png');
+				img.setAttribute('id', 'img');
+				img.setAttribute('style', 'transform:'+estilo);
+				//var value_TD = document.createTextNode(img);
+				document.getElementById(direccion).appendChild(img);
 }
