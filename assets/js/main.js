44998b0d02f2c84111477319a8ee9bb75e311fdc
Mariley20 <mariley2018@gmail.com>
2017-08-08 12:05:50 -0500

    comentarios1

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,10 +28,10 @@ for (var i = 0; i < mapa.length; i++) {
             clase = 'camino    ';
         } else if (pared[j] == "o") {
             clase = 'inicioFin';
-            var img = document.createElement('img');
+            var img = document.createElement('');
             img.setAttribute('src', 'assets/icn/flecha_arriba.png');
             img.setAttribute('id', 'img');
-            img.setAttribute('style', 'transform:rotate(360deg)')
+            img.setAttribute('style', 'transform:rotate(360deg)');
             //var value_TD = document.createTextNode(img);
             tag_TD.appendChild(img);
         } else {
@@ -99,8 +99,8 @@ function rotar(c) {
 var btnAvanzar = document.getElementById('avanzar');
 btnAvanzar.onclick = function () {
     var idTagPadre = document.getElementById('img').parentNode.id;
-    idTag = idTagPadre.split('');
-    
+    var idTag = idTagPadre.split('');
+
     if (idTag.length == 3) {
         idTag[1] = idTag[1] + "" + idTag[2];
     }
@@ -169,42 +169,80 @@ function removeImg(direccion, idTagPadre, estilo) {
     document.getElementById(direccion).appendChild(img);
 }
 
+
+///-----------
 function caminaSolito() {
     var idTagPadre = document.getElementById('img').parentNode.id;
-    idTag = idTagPadre.split('');
+    var idTag = idTagPadre.split('');
+
     if (idTag.length == 3) {
         idTag[1] = idTag[1] + "" + idTag[2];
     }
-    (idTagPadre == '916') ? alert('Ganaste'): console.log(idTagPadre)
-
-    var derecha = idTag[0] + "" + (parseInt(idTag[1]) + 1);
-    var izquierda = idTag[0] + "" + (parseInt(idTag[1]) - 1);
-    var arriba = (parseInt(idTag[0]) - 1) + "" + idTag[1];
-    var abajo = (parseInt(idTag[0]) + 1) + "" + idTag[1];
-
     var estilo = document.getElementById('img').style.transform;
-
-    var derechaValor = document.getElementById(derecha).textContent;
-    var izquierdaValor = document.getElementById(izquierda).textContent;
-    var arribaValor = document.getElementById(arriba).textContent;
-    var abajoValor = document.getElementById(abajo).textContent;
-    var direccion;
-    console.log(izquierdaValor + '*' + arribaValor);
-    if (izquierdaValor == '*' && arribaValor == "_") {
-        direccion = arriba;
-        removeImg(direccion, idTagPadre, estilo);
+    switch (estilo) {
+        case 'rotate(360deg)':
+            //avanza para ARRIBA
+            var izq_1 = idTag[0]+""+(parseInt(idTag[1]) - 1);
+            var izq_2 = (parseInt(idTag[0]) - 1)+""+(parseInt(idTag[1]) - 1);
+            subearriba = (parseInt(idTag[0]) - 1)+""+idTag[1];
+            var izq_V1 = document.getElementById(izq_1).textContent;
+            var izq_V2 = document.getElementById(izq_2).textContent;
+            var arribaValor = document.getElementById(subearriba).textContent;
+    
+            if((izq_V1 == "*" || izq_V1=='_') && izq_V2 == "*" && arribaValor == "_"){
+                removeImg(subearriba, idTagPadre, estilo);
+            }else if(izq_V1 == "*" && izq_V2 == "_" && arribaValor == "_"){
+                estilo = "rotate(270deg)";
+                removeImg(subearriba, idTagPadre, estilo);
+            }
+            break;
+        case 'rotate(90deg)':
+            //Avanza para bajo
+            izq_1 = (parseInt(idTag[0]) - 1) +""+ idTag[1];
+            izq_2 = (parseInt(idTag[0]) - 1) +""+ (parseInt(idTag[1]) + 1);
+            subeDerecha = idTag[0] +""+ (parseInt(idTag[1]) - 1);
+            var izq_V1 = document.getElementById(izq_1).textContent;
+            var izq_V2 = document.getElementById(izq_2).textContent;
+            var derechaValor = document.getElementById(subeDerecha).textContent;
+            if((izq_V1 == "*" || izq_V1 == "_") && izq_V2 == "*" && derechaValor == "_"){
+                removeImg(subeDerecha, idTagPadre, estilo);
+            }else if(izq_V1 == "*" && izq_V2 == "_" && derechaValor == "_"){
+                estilo = "rotate(360deg)";
+                removeImg(subeDerecha, idTagPadre, estilo);
+            }
+            
+            break;
+        case 'rotate(270deg)':
+            //avanza a la izquierda
+            izq_1 = (parseInt(idTag[0]) + 1) +""+ idTag[1];
+            izq_2 = (parseInt(idTag[0]) + 1) +""+ (parseInt(idTag[1]) - 1);
+            subeIzquierda = idTag[0] +""+ (parseInt(idTag[1]) - 1);
+            var izq_V1 = document.getElementById(izq_1).textContent;
+            var izq_V2 = document.getElementById(izq_2).textContent;
+            var izquierdaValor = document.getElementById(subeIzquierda).textContent;
+            if((izq_V1 == "*" || izq_V1 == "_") && izq_V2 == "*" && izquierdaValor == "_"){
+                removeImg(subeIzquierda, idTagPadre, estilo);
+            }else if(izq_V1 == "*" && izq_V2 == "_" && izquierdaValor == "_"){
+                estilo = "rotate(180deg)";
+                removeImg(subeIzquierda, idTagPadre, estilo);
+            }
+            
+            break;
+        case 'rotate(180deg)':
+            // avanza a la derecha
+            izq_1 = idTag[0] +""+ (parseInt(idTag[1]) + 1);
+            izq_2 = (parseInt(idTag[0]) + 1) +""+ (parseInt(idTag[1]) + 1);
+            bajar = (parseInt(idTag[0]) + 1) +""+ idTag[1];
+            var izq_V1 = document.getElementById(izq_1).textContent;
+            var izq_V2 = document.getElementById(izq_2).textContent;
+            var bajarCelda = document.getElementById(bajar).textContent;
+            if((izq_V1 == "*" || izq_V1 == "_") && izq_V2 == "*" && bajarCelda == "_"){
+                removeImg(bajar, idTagPadre, estilo);
+            }else if(izq_V1 == "*" && izq_V2 == "_" && bajarCelda == "_"){
+                estilo = "rotate(90deg)";
+                removeImg(bajar, idTagPadre, estilo);
+            }
+            
+            break;
     }
-    if (arribaValor == "*" && derechaValor == '_') {
-        direccion = derecha;
-        removeImg(direccion, idTagPadre, estilo);
-    }
-    if (derechaValor == '*' && abajoValor == '_') {
-        direccion = abajo;
-        removeImg(direccion, idTagPadre, estilo);
-    }
-    if (abajoValor == '*' && izquierdaValor == '_') {
-        direccion = arriba;
-        removeImg(direccion, idTagPadre, estilo);
-    }
-
 }
